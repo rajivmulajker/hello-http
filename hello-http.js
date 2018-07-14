abbaf0d4be58ff71a4c40ec1c09ae3e8d365a14a
Rajiv Mulajker <32780774+rajivmulajker@users.noreply.github.com>
2018-07-14 18:04:02 -0400

    Fixed Bugs

--- hello-http.js
+++ hello-http.js
@@ -2,7 +2,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
 
-    res.end('INITIAL Release !!! HELLO Host/Pod: ' + process.env.HOSTNAME  + '\n');
+    res.end('Fixed BUGS !!! HELLO Host/Pod: ' + process.env.HOSTNAME  + '\n');
 
 }).listen(8000, '0.0.0.0');
 console.log('Server running at http://localhost:8000/');
