46b23ad5641b8eee5b05aec0fcd56592e5ed1f3a
Rajiv Mulajker <32780774+rajivmulajker@users.noreply.github.com>
2018-07-14 17:56:43 -0400

    Add files via upload

---
+++ hello-http.js
@@ -0,0 +1,9 @@
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    res.end('INITIAL Release !!! HELLO Host/Pod: ' + process.env.HOSTNAME  + '\n');
+
+}).listen(8000, '0.0.0.0');
+console.log('Server running at http://localhost:8000/');
+
